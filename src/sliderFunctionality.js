ab24a088dea264311d9fb272b480e1a4eaba917b
GiacomoGitHub <poddagiacomo@gmail.com>
2021-07-01 12:45:38 +0300

    Redesigned the interface

--- src/sliderFunctionality.js
+++ src/sliderFunctionality.js
@@ -1,6 +1,22 @@
+// const sliderFunctionality = () => {
+// 	const goToNextImage = () => {};
+// 	const goToPreviousImage = () => {};
+// };
+
+// export default sliderFunctionality;
+
 const sliderFunctionality = () => {
-	const goToNext = () => {};
-	const goToPrevious = () => {};
+	let n = 0;
+	const goToNextImage = () => {
+		document.getElementById('next').addEventListener('click', () => {
+			n += 1;
+			console.log(n);
+		});
+		goToNextImage;
+	};
+	const goToPreviousImage = () => {
+		n -= 1;
+	};
 };
 
 export default sliderFunctionality;
